cb146fe34acfc7a8b7ef9ce2fe17ca62bececb19
Harry Pannu <thepannu786@gmail.com>
2021-05-03 17:19:08 +0530

    🥱

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 //Creating FUn functions  ===========>
 
@@ -17,15 +17,92 @@ function togglePlay() {
 
 function updateBtn() {
   const icon = this.paused ? '►' : '❚ ❚';
-  console.log(icon);
-
   toggle.textContent = icon;
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+
+if (!document.exitFullscreen) {
+  document.exitFullscreen =
+    document.mozExitFullscreen ||
+    document.webkitExitFullscreen ||
+    document.msExitFullscreen;
+}
+
+/**
+ * document.fullscreenElement polyfill
+ * Adapted from https://shaka-player-demo.appspot.com/docs/api/lib_polyfill_fullscreen.js.html
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!document.fullscreenElement) {
+  Object.defineProperty(document, 'fullscreenElement', {
+    get: function () {
+      return (
+        document.mozFullScreenElement ||
+        document.msFullscreenElement ||
+        document.webkitFullscreenElement
+      );
+    },
+  });
+
+  Object.defineProperty(document, 'fullscreenEnabled', {
+    get: function () {
+      return (
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled ||
+        document.webkitFullscreenEnabled
+      );
+    },
+  });
+}
+
+document.addEventListener(
+  'click',
+  function (event) {
+    // Ignore clicks that weren't on the toggle button
+    if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+
+    // If there's an element in fullscreen, exit
+    // Otherwise, enter it
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  },
+  false
+);
 //Hooking up functionality to buttons and screen ===========>
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateBtn);
 video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach((button) => button.addEventListener('click', skip));
+ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
